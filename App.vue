5518400758645d0b5b57b2e36bbab3950db35715
Bappaditya <bappaditya.sasmal@qlikchain.com>
2020-05-02 12:52:27 +0530

    another commit

--- App.vue
+++ App.vue
@@ -1,17 +1,33 @@
 <template>
-  <view class="container">
-    <text class="text-color-primary">My Vue Native App</text>
-  </view>
+    <view class="container">
+        <button :title="message" @press="handleBtnPress"/>
+    </view>
 </template>
 
+<script>
+    export default {
+        data: function() {
+            return {
+                message: "Hello World"
+            };
+        },
+        methods: {
+            handleBtnPress: function() {
+                alert('Btn Press');
+            }
+        }
+    };
+</script>
+
 <style>
-.container {
-  background-color: white;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-}
-.text-color-primary {
-  color: blue;
-}
+    .container {
+        background-color: white;
+        align-items: center;
+        justify-content: center;
+        flex: 1;
+    }
+
+    .text-color-primary {
+        color: blue;
+    }
 </style>
